6170349ea8bb1bdb1249fd73b3f2a345d5f178a7
miamollie <mia.deburca@99designs.com>
2020-04-17 21:02:20 +1000

    Tweak home page

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -2,10 +2,9 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
-import Content, { HTMLContent } from '../components/Content'
+import { HTMLContent } from '../components/Content'
 
-export const AboutPageTemplate = ({ title, content, contentComponent }) => {
-  const PageContent = contentComponent || Content
+export const AboutPageTemplate = ({ title, content }) => {
 
   return (
     <section className="section section--gradient">
@@ -16,7 +15,7 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
               <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
                 {title}
               </h2>
-              <PageContent className="content" content={content} />
+              <HTMLContent className="content" content={content} />
             </div>
           </div>
         </div>
@@ -37,7 +36,6 @@ const AboutPage = ({ data }) => {
   return (
     <Layout>
       <AboutPageTemplate
-        contentComponent={HTMLContent}
         title={post.frontmatter.title}
         content={post.html}
       />
