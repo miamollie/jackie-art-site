f9fd3862449cb902e6edc34a0b07ec7cd096e68c
miamollie <mia.deburca@vista.com>
2025-02-18 14:21:42 +1100

    Fix react peer deps to fix cms issue

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,12 +1,12 @@
-import CMS from 'decap-cms-app'
+import CMS from "decap-cms-app";
 
-import AboutPagePreview from './preview-templates/AboutPagePreview'
-import ProjectsPagePreview from './preview-templates/ProjectsPagePreview'
-import ProjectPagePreview from './preview-templates/ProjectPagePreview'
-import IndexPagePreview from './preview-templates/IndexPagePreview'
+import AboutPagePreview from "./preview-templates/AboutPagePreview";
+import ProjectsPagePreview from "./preview-templates/ProjectsPagePreview";
+import ProjectPagePreview from "./preview-templates/ProjectPagePreview";
+import IndexPagePreview from "./preview-templates/IndexPagePreview";
 
 
-CMS.registerPreviewTemplate('index', IndexPagePreview)
-CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('projects', ProjectsPagePreview)
-CMS.registerPreviewTemplate('project', ProjectPagePreview)
+CMS.registerPreviewTemplate("index", IndexPagePreview);
+CMS.registerPreviewTemplate("about", AboutPagePreview);
+CMS.registerPreviewTemplate("projects", ProjectsPagePreview);
+CMS.registerPreviewTemplate("project", ProjectPagePreview);
